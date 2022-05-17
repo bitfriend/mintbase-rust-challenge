f0a8d7ae16d4358efb483e2a936573c609c33820
bitfriend <bitcapture@hotmail.com>
2022-05-17 11:11:12 -0600

    Implement 1st task

--- src/task_i.rs
+++ src/task_i.rs
@@ -1,4 +1,18 @@
-pub struct StringWrapper;
+pub struct StringWrapper {
+    data: String,
+}
+
+impl StringWrapper {
+    pub fn from(value: &str) -> Self {
+        Self {
+            data: String::from(value),
+        }
+    }
+
+    pub fn as_ref(&self) -> &str {
+        return self.data.as_str().as_ref();
+    }
+}
 
 #[cfg(test)]
 mod tests {
